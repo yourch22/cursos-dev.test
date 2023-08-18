5575140b111fbd944ad797bf52de3f462381207b
yourch <33913578+yourch22@users.noreply.github.com>
2023-08-18 17:55:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cursos-dev
+ 
---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Serviciosapi;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    protected $serviciosapi;
+    use AuthorizesRequests, ValidatesRequests, ValidatesRequests;
+    public function __construct(Serviciosapi $serviciosapi)
+    {
+        $this->serviciosapi = $serviciosapi;
+    }
+}
